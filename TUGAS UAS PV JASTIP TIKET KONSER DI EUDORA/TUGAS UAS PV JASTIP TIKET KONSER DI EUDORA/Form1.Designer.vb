6201a990c979e19a40cb7417d79b3cb5e5ea2f2a
Nur Fadzilah <nurfadzilah0201@gmail.com>
2026-02-10 03:52:38 +0700

    Add files via upload

---
+++ TUGAS UAS PV JASTIP TIKET KONSER DI EUDORA/TUGAS UAS PV JASTIP TIKET KONSER DI EUDORA/Form1.Designer.vb
@@ -0,0 +1,99 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.lblJudul = New System.Windows.Forms.Label()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.dgvJastip = New System.Windows.Forms.DataGridView()
+        Me.btnBaca = New System.Windows.Forms.Button()
+        CType(Me.dgvJastip, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'lblJudul
+        '
+        Me.lblJudul.AutoSize = True
+        Me.lblJudul.BackColor = System.Drawing.Color.AliceBlue
+        Me.lblJudul.Font = New System.Drawing.Font("Segoe UI", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblJudul.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.lblJudul.Location = New System.Drawing.Point(219, 17)
+        Me.lblJudul.Name = "lblJudul"
+        Me.lblJudul.Size = New System.Drawing.Size(351, 30)
+        Me.lblJudul.TabIndex = 0
+        Me.lblJudul.Text = "JASTIP TIKET KONSER DI EUDORA"
+        '
+        'lblInfo
+        '
+        Me.lblInfo.AutoSize = True
+        Me.lblInfo.BackColor = System.Drawing.Color.AliceBlue
+        Me.lblInfo.Font = New System.Drawing.Font("Segoe UI", 14.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.lblInfo.Location = New System.Drawing.Point(3, 369)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(68, 25)
+        Me.lblInfo.TabIndex = 1
+        Me.lblInfo.Text = "TOTAL"
+        '
+        'dgvJastip
+        '
+        Me.dgvJastip.BackgroundColor = System.Drawing.Color.AliceBlue
+        Me.dgvJastip.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvJastip.Location = New System.Drawing.Point(1, 72)
+        Me.dgvJastip.Name = "dgvJastip"
+        Me.dgvJastip.Size = New System.Drawing.Size(804, 267)
+        Me.dgvJastip.TabIndex = 2
+        '
+        'btnBaca
+        '
+        Me.btnBaca.BackColor = System.Drawing.Color.AliceBlue
+        Me.btnBaca.Font = New System.Drawing.Font("Segoe UI", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnBaca.ForeColor = System.Drawing.Color.MidnightBlue
+        Me.btnBaca.Location = New System.Drawing.Point(614, 345)
+        Me.btnBaca.Name = "btnBaca"
+        Me.btnBaca.Size = New System.Drawing.Size(191, 29)
+        Me.btnBaca.TabIndex = 3
+        Me.btnBaca.Text = "BACA DATA JASTIP"
+        Me.btnBaca.UseVisualStyleBackColor = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LightBlue
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.btnBaca)
+        Me.Controls.Add(Me.dgvJastip)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.lblJudul)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.dgvJastip, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents lblJudul As Label
+    Friend WithEvents lblInfo As Label
+    Friend WithEvents dgvJastip As DataGridView
+    Friend WithEvents btnBaca As Button
+End Class
---
+++ TUGAS UAS PV JASTIP TIKET KONSER DI EUDORA/TUGAS UAS PV JASTIP TIKET KONSER DI EUDORA/Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports System.IO
+
+Public Class Form1
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Me.Text = "Jastip Tiket Konser"
+
+        lblJudul.Text = "JASTIP TIKET KONSER DI EUDORA"
+        lblJudul.Font = New Font("Segoe UI", 16, FontStyle.Bold)
+        lblJudul.ForeColor = Color.MidnightBlue
+
+        btnBaca.BackColor = Color.AliceBlue
+        btnBaca.ForeColor = Color.MidnightBlue
+
+        lblInfo.Font = New Font("Segoe UI", 14, FontStyle.Bold)
+        lblInfo.ForeColor = Color.MidnightBlue
+
+        With dgvJastip
+            .Columns.Clear()
+            .Columns.Add("nama", "Nama Pemesan")
+            .Columns.Add("jumlah", "Jumlah Tiket")
+            .Columns.Add("harga", "Harga")
+            .Columns.Add("total", "Total Bayar")
+            .Columns.Add("diskon", "Keterangan")
+
+            .AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill
+            .ReadOnly = True
+            .AllowUserToAddRows = False
+            .BorderStyle = BorderStyle.None
+
+            .EnableHeadersVisualStyles = False
+            .ColumnHeadersDefaultCellStyle.BackColor = Color.AliceBlue
+            .ColumnHeadersDefaultCellStyle.ForeColor = Color.MidnightBlue
+            .ColumnHeadersDefaultCellStyle.Font = New Font("Segoe UI", 12, FontStyle.Bold)
+
+            .DefaultCellStyle.Font = New Font("Segoe UI", 10)
+            .DefaultCellStyle.SelectionBackColor = Color.SteelBlue
+            .DefaultCellStyle.SelectionForeColor = Color.Black
+
+            .AlternatingRowsDefaultCellStyle.BackColor = Color.AliceBlue
+
+            .DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+            .ColumnHeadersDefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter
+        End With
+
+    End Sub
+
+
+    Private Sub btnBaca_Click(sender As Object, e As EventArgs) Handles btnBaca.Click
+
+        Dim ofd As New OpenFileDialog
+        ofd.Filter = "Text File|*.txt"
+
+        If ofd.ShowDialog() = DialogResult.OK Then
+
+            Dim baris() As String = File.ReadAllLines(ofd.FileName)
+
+            ProsesJastip(baris)
+
+        End If
+
+    End Sub
+
+
+    Sub ProsesJastip(data() As String)
+
+        Dim totalTransaksi As Integer = 0
+
+        dgvJastip.Rows.Clear()
+
+        For i As Integer = 0 To data.Length - 1
+
+            Dim pecah() As String = data(i).Split(",")
+
+            Dim nama As String = pecah(0)
+            Dim jumlah As Integer = Convert.ToInt32(pecah(1))
+            Dim harga As Integer = Convert.ToInt32(pecah(2))
+
+            Dim total As Integer = jumlah * harga
+            Dim infoDiskon As String = "Tidak ada diskon"
+
+            If jumlah >= 5 Then
+                total = total - 10000
+                infoDiskon = "Diskon jastip"
+            End If
+
+            totalTransaksi += total
+
+            dgvJastip.Rows.Add(
+                nama,
+                jumlah,
+                FormatRupiah(harga),
+                FormatRupiah(total),
+                infoDiskon
+            )
+
+        Next
+
+        lblInfo.Text = "Total seluruh transaksi : " & FormatRupiah(totalTransaksi)
+        lblInfo.Font = New Font("Segoe UI", 11, FontStyle.Bold)
+        lblInfo.ForeColor = Color.MidnightBlue
+
+    End Sub
+
+
+    Function FormatRupiah(angka As Integer) As String
+        Return "Rp " & angka.ToString("N0")
+    End Function
+
+End Class
+
